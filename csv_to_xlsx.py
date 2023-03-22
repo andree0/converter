ff00d8f00d5bfacc96b66cc80a4b10c2510cde69
Andrzej Jończy <andrzej.jonczy@mgbi.pl>
2023-03-22 22:06:07 +0100

    style: black

--- csv_to_xlsx.py
+++ csv_to_xlsx.py
@@ -2,19 +2,20 @@ import csv
 import os
 from xlsxwriter.workbook import Workbook
 
-delimiter = input("Podaj znak podziału: ")
-ending_files = {'.csv', '.CSV', '.txt', '.TXT'}
 
-files = [file for file in os.listdir('files_to_convert')]
+delimiter = input("Podaj znak podziału: ")
+ending_files = {".csv", ".CSV", ".txt", ".TXT"}
+
+files = [file for file in os.listdir("files_to_convert")]
 for file in files:
-    with open(f'files_to_convert/{file}', mode='r', encoding='utf-8') as csv_file:
-        #zły znak podziału powoduje tylko zmianę rozszerzenia na .xlsx
-        #do napisania funkcja wykrywająca znak podziału
+    with open(f"files_to_convert/{file}", mode="r", encoding="utf-8") as csv_file:
+        # zły znak podziału powoduje tylko zmianę rozszerzenia na .xlsx
+        # do napisania funkcja wykrywająca znak podziału
         read_file = csv.reader(csv_file, delimiter=delimiter)
         if (ending := file[-4:]) in ending_files:
-            new_file_name = file.replace(ending, '.xlsx')
+            new_file_name = file.replace(ending, ".xlsx")
             workbook = Workbook(f"converted_files/{new_file_name}")
-            worksheet = workbook.add_worksheet('dane')
+            worksheet = workbook.add_worksheet("dane")
             row = 0
             for line in read_file:
                 col = 0
